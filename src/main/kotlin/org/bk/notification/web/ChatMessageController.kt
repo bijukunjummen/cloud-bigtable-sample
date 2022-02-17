c9bc01b7a9505bfdea63ef320df5979f8d2e5d96
Biju Kunjummen <biju.kunjummen@gmail.com>
2022-02-16 17:14:00 -0800

    Minor cleanup

--- src/main/kotlin/org/bk/notification/web/ChatMessageController.kt
+++ src/main/kotlin/org/bk/notification/web/ChatMessageController.kt
@@ -4,6 +4,7 @@ import org.bk.notification.model.ChatMessage
 import org.bk.notification.service.ChatMessageHandler
 import org.bk.notification.web.model.ChatMessageRequest
 import org.springframework.http.ResponseEntity
+import org.springframework.util.StopWatch
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -50,5 +51,14 @@ class ChatMessageController(private val chatMessageHandler: ChatMessageHandler) 
             .switchIfEmpty(
                 Mono.just(ResponseEntity.notFound().build())
             )
+            .contextWrite { context ->
+                val stopWatch = StopWatch()
+                stopWatch.start()
+                context.put(STOPWATCH_KEY, stopWatch)
+            }
+    }
+
+    companion object {
+        private const val STOPWATCH_KEY = "stopWatchKey"
     }
 }
