94bd21e07bd0d2a0048abdfdeecd0802bdc2e0d0
Biju Kunjummen <biju.kunjummen@gmail.com>
2022-02-17 07:22:40 -0800

    Adds pooling

--- src/main/kotlin/org/bk/notification/config/BigtableConfiguration.kt
+++ src/main/kotlin/org/bk/notification/config/BigtableConfiguration.kt
@@ -4,6 +4,7 @@ import com.google.api.gax.core.NoCredentialsProvider
 import com.google.cloud.ServiceOptions
 import com.google.cloud.bigtable.data.v2.BigtableDataClient
 import com.google.cloud.bigtable.data.v2.BigtableDataSettings
+import com.google.cloud.bigtable.data.v2.stub.EnhancedBigtableStubSettings
 import org.bk.notification.service.BigtableChatMessageRepository
 import org.bk.notification.service.BigtableChatRoomRepository
 import org.springframework.context.annotation.Bean
@@ -15,19 +16,28 @@ class BigtableConfiguration {
     fun bigtableDataClient(bigtableProperties: BigtableProperties): BigtableDataClient {
         if (bigtableProperties.emulatorPort != 0) {
             return BigtableDataClient
-                    .create(BigtableDataSettings
-                            .newBuilderForEmulator(bigtableProperties.emulatorPort)
-                            .setProjectId(bigtableProperties.projectId)
-                            .setInstanceId(bigtableProperties.instanceId)
-                            .setCredentialsProvider(NoCredentialsProvider.create())
-                            .build())
-        }
-        return BigtableDataClient
-                .create(BigtableDataSettings
-                        .newBuilder()
+                .create(
+                    BigtableDataSettings
+                        .newBuilderForEmulator(bigtableProperties.emulatorPort)
+                        .setProjectId(bigtableProperties.projectId)
                         .setInstanceId(bigtableProperties.instanceId)
-                        .setProjectId(ServiceOptions.getDefaultProjectId())
-                        .build())
+                        .setCredentialsProvider(NoCredentialsProvider.create())
+                        .build()
+                )
+        }
+        val settingsBuilder: BigtableDataSettings.Builder = BigtableDataSettings
+            .newBuilder()
+            .setInstanceId(bigtableProperties.instanceId)
+            .setProjectId(ServiceOptions.getDefaultProjectId())
+
+        settingsBuilder
+            .stubSettings()
+            .setTransportChannelProvider(
+                EnhancedBigtableStubSettings.defaultTransportChannelProvider().withPoolSize(10)
+            )
+
+        return BigtableDataClient
+            .create(settingsBuilder.build())
     }
 
     @Bean
