0ff6667f87ce7aa381a77ad2c8f793421e5c8644
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2022-03-28 12:44:21 -0700

    Adds a basic load test

--- src/main/kotlin/org/bk/notification/web/ChatMessageController.kt
+++ src/main/kotlin/org/bk/notification/web/ChatMessageController.kt
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import java.time.Instant
 import java.util.*
 
@@ -62,12 +64,58 @@ class ChatMessageController(private val chatMessageHandler: ChatMessageHandler) 
             }
     }
 
+    @GetMapping(path = ["/load/{chatRoomId}/{chatMessageId}"])
+    fun performLoadTest(
+        @RequestParam(name = "concurrent", defaultValue = "2") concurrent: Int,
+        @RequestParam(name = "count", defaultValue = "100") count: Int,
+        @PathVariable("chatRoomId") chatRoomId: String,
+        @PathVariable("chatMessageId") chatMessageId: String
+    ): Mono<ResponseEntity<PerfData>> {
+        return Flux.range(1, count)
+            .parallel(concurrent)
+            .runOn(PAR_SCHEDULER)
+            .flatMap { c ->
+                chatMessageHandler
+                    .getChatMessage(chatRoomId, chatMessageId)
+                    .flatMap { chatMessage ->
+                        Mono.deferContextual { context ->
+                            val stopWatch = context.get<StopWatch>(STOPWATCH_KEY)
+                            stopWatch.stop()
+                            Mono.just(stopWatch.totalTimeMillis)
+                        }
+                    }
+                    .contextWrite { context ->
+                        val stopWatch = StopWatch()
+                        stopWatch.start()
+                        context.put(STOPWATCH_KEY, stopWatch)
+                    }
+            }
+            .sequential()
+            .collectList()
+            .map { list ->
+                list.sort()
+                val map: LinkedHashMap<Int, Long> = listOf(50, 75, 95, 99)
+                    .map { percentile ->
+                        val index = Math.ceil(percentile / 100.0 * list.size).toInt()
+                        percentile to list.get(index - 1)
+                    }
+                    .associateByTo(LinkedHashMap(), { pair -> pair.first }, { pair -> pair.second })
+                ResponseEntity.ok(PerfData(list.size, map))
+            }
+    }
+
     companion object {
         private const val STOPWATCH_KEY = "stopWatchKey"
+        private val PAR_SCHEDULER = Schedulers.newParallel("par", 10)
     }
 }
 
 data class WithTimeWrapper<T>(
     val data: T,
     val timeElapsedMillis: Long
+)
+
+data class PerfData(
+    val sampleSize: Int,
+    val percentileToValue: LinkedHashMap<Int, Long>
 )
